5db90b45647efb655b1de87b672b69719a835ef1
Tom Swope <tjswope@gmail.com>
2022-11-21 08:25:58 -0500

    Added ProductList class

--- New Wave Store Set 3/Model/Product.swift
+++ New Wave Store Set 3/Model/Product.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-class Product{
-    var name: String
-    var description: String
-    var picture: String
-    var price: Double
-    var quantity: Int
+class Product: ObservableObject{
+    @Published var name: String
+    @Published var description: String
+    @Published var picture: String
+    @Published var price: Double
+    @Published var quantity: Int
     
     init(name: String = "NW-PC", description: String = "New Wave's Worst Computer", picture: String = "NW-PC", price: Double = 9999.99, quantity: Int = 100){
         self.name = name
