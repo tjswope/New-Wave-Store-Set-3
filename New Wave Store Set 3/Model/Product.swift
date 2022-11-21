3e52ec74d2453a9cf79ef7000506ad497223d290
Tom Swope <tjswope@gmail.com>
2022-11-21 08:07:23 -0500

    added images, icon set and Product class

---
+++ New Wave Store Set 3/Model/Product.swift
@@ -0,0 +1,24 @@
+//
+//  Product.swift
+//  New Wave Store
+//
+//  Created by Swope, Thomas on 11/18/22.
+//
+
+import Foundation
+
+class Product{
+    var name: String
+    var description: String
+    var picture: String
+    var price: Double
+    var quantity: Int
+    
+    init(name: String = "NW-PC", description: String = "New Wave's Best Computer", picture: String = "NW-PC", price: Double = 9999.99, quantity: Int = 100){
+        self.name = name
+        self.description = description
+        self.picture = picture
+        self.price = price
+        self.quantity = quantity
+    }
+}
